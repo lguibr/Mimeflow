bada6ba6f7c251737810fa64f251e2584b738294
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2025-12-04 19:37:25 -0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -20,17 +20,17 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
